37db997e1ec9dfa9cb2e4bad8d3d1b0cdd6c0958
joeyj364 <71462893+joey364@users.noreply.github.com>
2021-10-06 15:41:43 +0000

    Add comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+// Import express module
 const express = require('express')
 
+// Instantiates express app
 const app = express()
 
-app.get('/', (req, res, next) => {
+// Routes
+app.get('/', (req, res) => {
   res.json({ messaege: 'there are people here' })
 })
 
+//Start the express app
 app.listen(3000, () => {
   console.log('server is up')
 })
