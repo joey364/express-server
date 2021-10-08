108b434f7f8eee3e1bf876175582c24c9150d4e7
joeyj364 <71462893+joey364@users.noreply.github.com>
2021-10-08 13:58:11 +0000

    Add middleware and comments

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ const express = require('express')
 // Instantiates express app
 const app = express()
 
+// Middleware
+// parses request body to json
+app.use(express.json())
+
+// parses urlencoded query string
+app.use(express.urlencoded({ extended: true }))
+
 // Routes
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.json({ messaege: 'there are people here' })
+})
+
+app.post('/product', (req, res) => {
+  console.log(req.body)
+  res.send('Data don reach here')
 })
 
 //Start the express app
